70127b382178108fd220f00089d621dfbf721605
mischaaf <49269276+mischaaf@users.noreply.github.com>
2019-05-15 20:49:27 +0200

    Add files via upload

---
+++ 03_trials.js
@@ -0,0 +1,63 @@
+// In this file you can specify the trial data for your experiment
+
+
+const trial_info = {
+    key_press: [
+        {
+            question: "Are the objects same or different?",
+            picture: "images/1_50_different.jpg",
+            picture_number: 1,
+            degree: 50,
+            key1: 'b',
+            key2: 'n',
+            b: 'same',
+            n: 'different',
+            expected: 'different'
+        },
+        {
+            question: "Are the objects same or different?",
+            picture: "images/1_50_same.jpg",
+            picture_number: 2,
+            degree: 50,
+            key1: 'b',
+            key2: 'n',
+            b: 'same',
+            n: 'different',
+            expected: 'same'
+        },
+        {
+            question: "Are the objects same or different?",
+            picture: "images/1_150_different.jpg",
+            picture_number: 3,
+            degree: 150,
+            key1: 'b',
+            key2: 'n',
+            b: 'same',
+            n: 'different',
+            expected: 'different'
+        },
+        {
+            question: "Are the objects same or different?",
+            picture: "images/1_150_same.jpg",
+            picture_number: 4,
+            degree: 150,
+            key1: 'b',
+            key2: 'n',
+            b: 'same',
+            n: 'different',
+            expected: 'same'
+        },
+        {
+            question: "Are the objects same or different?",
+            picture: "images/2_50_different.jpg",
+            picture_number: 5,
+            degree: 50,
+            key1: 'b',
+            key2: 'n',
+            b: 'same',
+            n: 'different',
+            expected: 'different'
+        }
+
+    ],
+};
